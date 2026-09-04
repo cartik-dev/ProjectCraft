c8e2eee9c626679fc4c4a3d0d7ee7e4a099adf8a
cartik <010213pashaleshenko1@gmail.com>
2026-09-04 13:43:00 +0500

    feat: restore mobile recipe book overlay

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { GameViewport } from './components/GameViewport';
+import { RecipeBookMobile } from './components/RecipeBookMobile';
 
 export const App: React.FC = () => (
   <main style={{ width: '100vw', height: '100vh', position: 'relative', overflow: 'hidden' }}>
     <GameViewport />
+    <RecipeBookMobile />
   </main>
 );
 
